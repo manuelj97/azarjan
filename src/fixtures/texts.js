f0f24634045daae984a72d193f072ec5e17becbe
manuelj97 <34404371+manuelj97@users.noreply.github.com>
2021-12-23 09:43:27 +0100

    Create texts.js

---
+++ src/fixtures/texts.js
@@ -0,0 +1,16 @@
+const texts = [
+    { 
+        id:1,
+        heading:"Skills",
+        title: "Javascript (React) · HTML · CSS · Vector graphics",
+        subtitle: "I focus on my personal projects to better my React knowledge for my junior lever experience. I have previosly coded in C# and Java. I have equired those skills during my years in high school, where I graduated with a diploma, focused on programming. I also have knowledge in digital illustration and graphic design."
+    },
+    {
+        id:2,
+        heading:"Work Experience",
+        title: "System Analyst and Software Tester in Česká spořitelna and Octopus Newsroom (3 years of experience)",
+        subtitle: "I was responsible for preparing and organising the development of a banking software. Oftentimes, something had to be changed in the software and I had to conceptualize the ideas from the clients. These changes were either cosmetic or functional. Some of my work consisted of designing the look of the software. Performed analysis and design including investigations, documentation, recommendation, and problem-solving. Wrote user requirements into technical specifications. I worked in an agile software development enviroment"
+    }
+]
+
+export default texts
